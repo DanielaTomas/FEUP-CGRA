80403ca7ac33b92871b7936db2f542efba38edcf
Diogo Nunes <diogoffn@gmail.com>
2023-02-23 16:17:01 +0000

    removed unnecesarry code

--- tp2/MyInterface.js
+++ tp2/MyInterface.js
@@ -20,10 +20,10 @@ export class MyInterface extends CGFinterface {
         //Checkbox elements in GUI
         this.gui.add(this.scene, 'displayAxis').name('Display Axis');
         this.gui.add(this.scene, 'displayDiamond').name('Display Diamond');
-        this.gui.add(this.scene, 'displayTriangle').name('Display Triangle');
+        /*this.gui.add(this.scene, 'displayTriangle').name('Display Triangle');
         this.gui.add(this.scene, 'displayParallelogram').name('Display Parallelogram');
         this.gui.add(this.scene, 'displayTriangleSmall').name('Display Small Triangle');
-        this.gui.add(this.scene, 'displayTriangleBig').name('Display Big Triangle');
+        this.gui.add(this.scene, 'displayTriangleBig').name('Display Big Triangle');*/
 
         //Slider element in GUI
         this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
